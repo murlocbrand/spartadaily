bcad41ed55191a3defd2ec05529d923b0fbdefbf
Axel Smeets <murlocbrand@gmail.com>
2016-03-31 14:02:56 +0200

    impl basic server

---
+++ news/server.cc
@@ -0,0 +1,52 @@
+/* server.cc: sample server program */
+#include "server.h"
+#include "connection.h"
+#include "connectionclosedexception.h"
+
+#include "service.h"
+
+#include <memory>
+#include <iostream>
+#include <string>
+#include <stdexcept>
+#include <cstdlib>
+
+using namespace std;
+
+int main(int argc, char* argv[]){
+    if (argc != 2) {
+        cerr << "Usage: myserver port-number" << endl;
+        exit(1);
+    }
+    
+    int port = -1;
+    try {
+        port = stoi(argv[1]);
+    } catch (exception& e) {
+        cerr << "Wrong port number. " << e.what() << endl;
+        exit(1);
+    }
+    
+    Server server(port);
+    if (!server.isReady()) {
+        cerr << "Server initialization error." << endl;
+        exit(1);
+    }
+    
+    Service service;
+    while (true) {
+        auto conn = server.waitForActivity();
+        if (conn != nullptr) {
+            try {
+                service.process(conn);
+            } catch (ConnectionClosedException&) {
+                server.deregisterConnection(conn);
+                cout << "Client closed connection" << endl;
+            }
+        } else {
+            conn = make_shared<Connection>();
+            server.registerConnection(conn);
+            cout << "New client connects" << endl;
+        }
+    }
+}
